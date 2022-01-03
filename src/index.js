3e4e2c8c517312ec8a74666857a23fe5174951ca
ianquill <ian@ianquill.com>
2022-01-03 12:54:24 -0800

    Moved update function to userInterface

--- src/index.js
+++ src/index.js
@@ -1,17 +1,2 @@
 import './style.css';
-import WebInterface from './webInterface';
-import updateFields from './userInterface';
-
-const body = document.body; 
-
-const webby = new WebInterface();
-
-async function update() {
-    const currentWeather = await webby.getCurrentWeather();
-    console.log('currentWeather: ' + currentWeather);
-    const forecast = await webby.getForecast(); 
-    console.log('forecast: ' + forecast);
-    updateFields(currentWeather, forecast);
-}
-
-update();
+import './userInterface';
--- src/userInterface.js
+++ src/userInterface.js
@@ -1,18 +1,30 @@
+import WebInterface from "./webInterface";
+
 const body = document.body;
 const form = document.querySelector('form');
 const search = document.querySelector('input');
 const currentWeatherContainer = document.getElementById('current-weather');
 const forecastContainer = document.getElementById('forecast');
 
-let searchIsValid = false;
+const webby = new WebInterface();
+
+async function update() {
+    const currentWeather = await webby.getCurrentWeather();
+    console.log('currentWeather: ' + currentWeather);
+    const forecast = await webby.getForecast(); 
+    console.log('forecast: ' + forecast);
+    updateFields(currentWeather, forecast);
+}
+
+update();
+
 
 function parseSearch() {
     if (search.value.length == 5) {
         console.log('valid length zip code');
-        searchIsValid = true;
+        update();
     } else {
         console.log('invalid input. try a zip code');
-        searchIsValid = false;
     }
 }
 
