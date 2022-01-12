591098147ecba15d57c8f47477cc1a204234afc8
ianquill <ian@ianquill.com>
2022-01-12 10:59:45 -0800

    Fixed icons and weather not updating

--- src/jsonUtils.js
+++ src/jsonUtils.js
@@ -2,11 +2,14 @@ function convertKelvintoF(temp) {
     return (temp * 9/5 - 459.67);
 }
 
+// utility function to convert unix date to standard format
 function unixToDate(date) {
     const ms = date * 1000;
     return new Date(ms).toLocaleString();
 }
 
+// strips unnecessary data from json response,
+// formats them, putting current + daily + hourly into a singular object
 export function processWeather(file) {
    console.log(file);
    const current = file.current;
@@ -15,15 +18,12 @@ export function processWeather(file) {
    const currentWeather = {
        // send this as another argument from geocodio?
        // location: file.name,
-       alerts: file.alerts.event,  
+       alerts: file.alerts[0].event,  
        date: unixToDate(current.dt),
        sunrise: unixToDate(current.sunrise),
        sunset: unixToDate(current.sunset),
        temperature: Math.round(convertKelvintoF(current.temp)) + ' F',
        feelsLike: Math.round(convertKelvintoF(current.feels_like)) + ' F',
-
-    //    tempMax: Math.round(convertKelvintoF(file.main.temp_max)) + ' F high',
-    //    tempMin: Math.round(convertKelvintoF(file.main.temp_min)) + ' F low',
        pressure: current.pressure + ' hPa',
        humidity: current.humidity + '% humidity',
        weather: current.weather[0].main,
@@ -41,8 +41,8 @@ export function processWeather(file) {
            tempMax: Math.round(convertKelvintoF(daily[i].temp.max)),
            tempMin: Math.round(convertKelvintoF(daily[i].temp.min)),
            pressure: daily[i].pressure,
-           weather: daily[i].weather.main,
-           icon: daily[i].weather.icon
+           weather: daily[i].weather[0].main,
+           icon: daily[i].weather[0].icon
        }
        dailyWeather.push(dayForecast);
    };
@@ -55,38 +55,15 @@ export function processWeather(file) {
             time: unixToDate(hourly[i].dt),
             temperature: hourly[i].temp,
             humidity: hourly[i].humidity,
-            weather: hourly[i].weather.main,
-            icon: hourly[i].weather.icon
+            weather: hourly[i].weather[0].main,
+            icon: hourly[i].weather[0].icon
         }
         hourlyWeather.push(hourForecast);
-   }
+   };
 
 
    const weatherData = [currentWeather, dailyWeather, hourlyWeather];
 
    return weatherData;
     
-}
-
-// wraps forecast in object
-export function processForecast(file) {
-    const forecast = [];
-
-    for (let i = 0; i < file.list.length; i++) {
-        const moment = {
-            date: unixToDate(file.list[i].dt),
-            temperature: Math.round(convertKelvintoF(file.list[i].main.temp)),
-            pressure: file.list[i].main.pressure + ' hPa',
-            humidity: file.list[i].main.humidity + '% humidity',
-            weatherMain: file.list[i].weather[0].main,
-            weatherDescription: file.list[i].weather[0].description,
-        }
-        forecast.push(moment);
-    }
-
-    for (let j = 0; j < forecast.length; j++) {
-        console.log(forecast[j]);        
-    }
-
-    return forecast;
 }
