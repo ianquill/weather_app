31e33574d39556f798e006834bccd3732be0069a
Ian Quill <ian@ianquill.com>
2022-01-13 14:34:43 -0800

    Daily and hourly forecasts now render on DOM

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # Todo
-CURRENTLY: 
+CURRENTLY: cleaning up hourly + daily formatting while tackling overall CSS
 
 [x] - parse search input
     [x] - updates existing fields, doesn't create new ones unless they don't currently exist
-    [ ] - process date with Date/Time API?
+    [?] - process date with Date/Time API?
         [x] - fetch icons from weather data
     [x] - if found automatically, reverse geocode to get a location name
 [x] - switch to one-call API?
     [x] - get location via location API 
     [x] - geocodio API convert to lat lon
-    [ ] - hourly and daily fields
-[ ] - add AQI API call (if more info needed somewhere)
-[ ] - animate color change of background for weather changes?
+    [x] - hourly and daily fields
+[?] - add AQI API call (if more info needed somewhere)
+[?] - animate color change of background for weather changes?
 [x] - front end design
     [ ] - implement css
 [ ] - build proper timeline, start with search bar, wait until everything is loaded before presenting it to the user
--- src/userInterface.js
+++ src/userInterface.js
@@ -23,6 +23,9 @@ currentWeatherContainer.style.visibility = 'hidden';
 // *** SETUP DAILY ***
 const dailyWeatherContainer = document.getElementById('daily');
 
+// setup hourly
+const hourlyWeatherContainer = document.getElementById('hourly');
+
 initialLoad();
 
 function getGeoLocation() {
@@ -65,6 +68,7 @@ export default async function updateFields(weather, locationName) {
     const daily = weather[1];
     const hourly = weather[2];
 
+    // current
     const dateTime = current.date.split(',');
     console.log(locationName);
     currentLocation.textContent = (
@@ -83,8 +87,61 @@ export default async function updateFields(weather, locationName) {
         currentWeatherContainer.style.visibility = 'visible';
     }
     currentIcon.src = `http://openweathermap.org/img/wn/${current.icon}@4x.png`;
-    // icon needs to be loaded, etc.
 
+    // hourly
+    for (let j = 0; j< hourly.length; j++) {
+        const icon = new Image();
+        icon.classList.add('hour-icon');
+        icon.src = `http://openweathermap.org/img/wn/${hourly[j].icon}.png`;
+        const container = document.createElement('div');
+        container.classList.add('hour-container');
+        const time = document.createElement('h3');
+        time.classList.add('hour-time');
+        const temp = document.createElement('h3');
+        temp.classList.add('hour-small');
+        const humidity = document.createElement('h3'); 
+        humidity.classList.add('hour-humidity');
+        const conditions = document.createElement('h3'); 
+        conditions.classList.add('hour-conditions');
 
+        let timeString = hourly[j].time.split(',');
+        timeString = timeString[1];
+        timeString = timeString.split(':');
+        timeString = `${timeString[0]}:${timeString[1]} ${timeString[2].split(' ')[1]}`;
+        time.textContent = timeString;
+
+        temp.textContent = hourly[j].temperature;
+        humidity.textContent = hourly[j].humidity;
+        conditions.textContent = hourly[j].weather;
+
+        container.append(time, temp, humidity, conditions, icon);
+        hourlyWeatherContainer.appendChild(container);
+    }
+
+    // daily
+    for (let i = 0; i < daily.length; i++) {
+        // setup daily template
+        const container = document.createElement('div');
+        container.classList.add('day-container');
+        const icon = new Image();
+        icon.src = `http://openweathermap.org/img/wn/${daily[i].icon}@2x.png`;
+
+        const textContainer = document.createElement('div');
+        textContainer.classList.add('day-text');
+        const date = document.createElement('h4');
+        date.classList.add('day-date', 'day-small');
+        const temp = document.createElement('h1');
+        temp.classList.add('day-temperature');
+        const conditions = document.createElement('h4');
+        conditions.classList.add('day-description');
+
+        date.textContent = daily[i].date;
+        temp.textContent = daily[i].temperature;
+        conditions.textContent = daily[i].weather;
+
+        textContainer.append(date, temp, conditions);
+        container.append(icon, textContainer);
+        dailyWeatherContainer.appendChild(container);
+    }
 };
 
